eda6625bc4a2b928bd2348892851c1b6f59a1c76
kelvinmburu <mburuhkelvin@gmail.com>
2022-06-13 23:09:59 +0300

    feat: imported functions from views

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
-from . import views
 from rest_framework import routers
+from . import views
 
 router = routers.DefaultRouter()
 router.register('users', views.UserViewSet)
